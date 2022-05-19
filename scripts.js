882306137cf883ee8df280d69364510ab74a3987
Ana Silva <anna.eu.bia@gmail.com>
2022-05-19 10:58:44 -0300

    Início da lógica do form

--- scripts.js
+++ scripts.js
@@ -15,3 +15,43 @@
 //     alert($("#nomeUsr").val());
 // });
 
+// let parede = {
+//     'altura': 0,
+//     'largura': 0,
+//     'nmrPortas': 0,
+//     'nmrJanelas': 0
+// }
+
+let comodo = [];
+let paredeAtual = 1;
+
+function atualizaParedeAtual() {
+    if (comodo.length > 0) {
+        paredeAtual = comodo.length + 1;
+    }
+
+    $('#textParede').text("parede " + paredeAtual);
+}
+
+$(document).ready(() => {
+    atualizaParedeAtual();
+});
+
+$('#formButton').on('click', (e) => {
+    e.preventDefault();
+
+    let altura = $('#altura').val();
+    let largura = $('#largura').val();
+    let nmrPortas = $('#portas').val();
+    let nmrJanelas = $('#janelas').val();
+
+    let area = altura * largura;
+
+    if (area >= 1 && area <= 15) {
+        alert("Muito bom meu garoto!!!");
+    }
+    else {
+        alert("Ruim");
+    }
+});
+
